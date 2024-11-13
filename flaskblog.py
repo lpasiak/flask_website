0fa6ccfada77cabdbf1f8a103622b5d9e11dec52
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2024-11-13 02:04:41 +0100

    Add dummy data (posts)

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Łukasz Pasiak',
+        'title': 'Blog Post 1',
+        'content': 'First post content.',
+        'date_posted': 'November 12, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content.',
+        'date_posted': 'November 13, 2024'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
